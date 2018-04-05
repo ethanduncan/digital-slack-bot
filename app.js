22482ee676493a1bd34778242a160f340d0c8434
ethanduncan <ethan.duncan13@gmail.com>
2018-04-05 21:36:03 +0100

    Initial commit

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
-const express = require('express')
-const app = express()
-const {google} = require('googleapis')
-const calendar = google.calendar('v3')
+const express = require('express');
+const app = express();
+var {google} = require('googleapis');
+var {slackBot} = require('./slackBot.js');
+const config = require("./config.js");
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
 
-const API_KEY = 'AIzaSyCojevKT5t_L7H75g6ysZFAjx9pX8Zi8rU'
+//Using a service account for bot use
+let privatekey = require('./private_key.json')
 
-var resp = calendar.calendars.get({
-    auth: API_KEY,
-    calendarId: 'ethan.duncan13@gmail.com'
-},(err,response) => {
-    if(err){
-        console.error(err)
-        return err
-    }
-    console.log(response)
-    return response
-})
+// configure a JWT auth client
+let jwtClient = new google.auth.JWT(
+  privatekey.client_email,
+  null,
+  privatekey.private_key,
+  ['https://www.googleapis.com/auth/calendar']);
+//authenticate request
+    jwtClient.authorize(function (err, tokens) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            console.log("Successfully connected!");
+     }
+    });
 
-app.get('/', (req, res) => res.send(resp))
+//Get todays date
+var today = new Date()
+console.log(timeMin)
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var timeMin = new Date(today.getFullYear(), today.getMonth(), today.getDate(),00,00,00,00)
+
+var timeMax = new Date(today.getFullYear(), today.getMonth(), today.getDate(),24,59,00,00)
+console.log(timeMax)
+
+//Google Calendar API
+let calendar = google.calendar('v3');
+
+calendar.events.list({ auth: jwtClient, calendarId: config.email, timeMax: timeMax, timeMin: timeMin }, function(err, resp) {
+    resp.data.items.forEach(function(cal) {
+        var message = cal.summary + " - " + cal.id + " - " + cal.creator.displayName;
+        console.log(message)
+        // slackBot.sendNotification(message);
+    });
+});
+
+app.get('/', (req, res) => res.send('resp'))
+
+app.listen(3000, () => console.log('listening on port 3000!'))
---
+++ slackBot.js
@@ -0,0 +1,16 @@
+const { IncomingWebhook, WebClient } = require('@slack/client');
+const config = require("./config.js")
+
+const notification = new IncomingWebhook(config.webhook);
+const currentTime = new Date().toTimeString();
+
+module.exports.sendNotification = function (message){
+    // notification.send(message, (error, resp) => {
+    // if (error) {
+    //     return console.error(error);
+    // }
+    // return console.trace()
+    //     });
+    console.log('test')
+}
+
